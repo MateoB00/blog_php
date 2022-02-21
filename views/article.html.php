8dea106f1ce3c0ec3ad605a4f527191f18b56749
Mateo Bianco <mateobianco91100@gmail.com>
2022-02-21 15:56:32 +0100

    bloquerAffCommentaire

--- views/article.html.php
+++ views/article.html.php
@@ -18,22 +18,25 @@
     <div>Date du commentaire : <strong><?= mb_strtoupper($commentaire->date_publication) ?></strong></div></br>
 <?php } ?>
 
-<form method="post" action=" <?= url('ajoutCommentaire' . '&id=' . $_GET['id']) ?>">
-    <?php if (!empty($error)) { ?>
-        <div class=" alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>Oups !</strong> <?php echo $error ?>
-        </div>
-    <?php } ?>
 
-    <div class="form-group row">
-        <label for="commentaireContenu" class="col-12 col-form-label">Contenu :</label>
-        <div class="col-12">
-            <input type="text" class="form-control" name="commentaireContenu" id="commentaireContenu" placeholder="Votre Contenu" required autofocus>
+<?php if (!empty($_SESSION['id'])) {    ?>
+    <form method="post" action=" <?= url('ajoutCommentaire' . '&id=' . $_GET['id']) ?>">
+        <?php if (!empty($error)) { ?>
+            <div class=" alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Oups !</strong> <?php echo $error ?>
+            </div>
+        <?php } ?>
+
+        <div class="form-group row">
+            <label for="commentaireContenu" class="col-12 col-form-label">Contenu :</label>
+            <div class="col-12">
+                <input type="text" class="form-control" name="commentaireContenu" id="commentaireContenu" placeholder="Votre Contenu" required autofocus>
+            </div>
         </div>
-    </div>
-    <div class="form-group row">
-        <button type="submit" class="btn btn-primary">Envoyer</button>
-    </div>
-</form>
+        <div class="form-group row">
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </div>
+    </form>
+<?php } ?>
 <?php require_once DOSSIER_VIEW . "/parties/footer.html.php";
 ?>
