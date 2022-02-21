9d95c83038efe2a36ec717a94a34e5fb8002bbc4
Mateo Bianco <mateobianco91100@gmail.com>
2022-02-21 12:43:25 +0100

    sécuritéBack - roleAdmin

--- controllers/modif-article-controller.php
+++ controllers/modif-article-controller.php
@@ -8,29 +8,34 @@ $article = Article::retrieveByPK($_GET['id']);
 
 if (empty($article)) error();
 
-if (!empty($_POST)) {
+if ($_SESSION['role'] == "admin") {
 
-    if (
-        !empty($_POST['titre'])
-        && !empty($_POST['contenu'])
-        && !empty($_POST['image'])
-        && filter_var($_POST['image'], FILTER_VALIDATE_URL) !== false
-        && !empty($_POST['auteur'])
-        && !empty($_POST['date_de_publication'])
-        && validateDate($_POST['date_de_publication'])
-        && (substr($_POST['image'], -4) == '.jpg' || substr($_POST['image'], -4) == '.png' || substr($_POST['image'], -5) == '.jpeg' || substr($_POST['image'], -4) == '.gif')
 
-    ) {
-        $article->titre = $_POST['titre'];
-        $article->contenu = $_POST['contenu'];
-        $article->image = $_POST['image'];
-        $article->auteur = $_POST['auteur'];
-        $article->date_de_publication = $_POST['date_de_publication'];
+    if (!empty($_POST)) {
 
-        $article->save();
+        if (
+            !empty($_POST['titre'])
+            && !empty($_POST['contenu'])
+            && !empty($_POST['image'])
+            && filter_var($_POST['image'], FILTER_VALIDATE_URL) !== false
+            && !empty($_POST['auteur'])
+            && !empty($_POST['date_de_publication'])
+            && validateDate($_POST['date_de_publication'])
+            && (substr($_POST['image'], -4) == '.jpg' || substr($_POST['image'], -4) == '.png' || substr($_POST['image'], -5) == '.jpeg' || substr($_POST['image'], -4) == '.gif')
 
-        redirection("home");
-    } else error();
+        ) {
+            $article->titre = $_POST['titre'];
+            $article->contenu = $_POST['contenu'];
+            $article->image = $_POST['image'];
+            $article->auteur = $_POST['auteur'];
+            $article->date_de_publication = $_POST['date_de_publication'];
+
+            $article->save();
+
+            redirection("home");
+        } else error();
+    }
+} else if (empty($_SESSION['role'])) {
+    $error = "Qu'est ce que vous faites là ?";
 }
-
 require_once DOSSIER_VIEW . '/modif-article.html.php';
--- controllers/suppr-article-controller.php
+++ controllers/suppr-article-controller.php
@@ -4,6 +4,8 @@ require_once DOSSIER_MODEL . '/Article.php';
 $article = Article::retrieveByPK($_GET['id']);
 
 if (empty($article)) error();
-$article->delete();
 
-redirection("liste-articles");
+if ($_SESSION['role'] == "admin") {
+    $article->delete();
+    redirection("liste-articles");
+} else redirection("liste-articles");
